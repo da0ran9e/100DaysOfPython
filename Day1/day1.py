96e66c28398c5a02403a06ad9dac12dea470cc90
Vũ Đức Sơn <vuducson1890@gmail.com>
2025-09-12 23:53:42 +0700

    Create day1.py

---
+++ Day1/day1.py
@@ -0,0 +1,16 @@
+
+print("India", "Vietnam", 6, sep='/')
+print("hello")
+print("world")
+print("hello", end=' ')
+print("world")
+print("hello\tWorld")
+print("i knew :\"python ain't fun!\"")
+print("""this is 
+a multi-line
+string""")
+name = "son"
+age = 18
+print(" my name is %s and im %d years old" %(name, age))
+print(" my name is {} and im {} years old".format(name,age))
+print(f"my name is {name} and im {age} years old")
