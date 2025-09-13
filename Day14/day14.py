890c835af08f731553342749269620d5d93c6bf3
Vũ Đức Sơn <vuducson1890@gmail.com>
2025-09-13 22:39:25 +0700

    wowwwwwww

---
+++ Day14/day14.py
@@ -0,0 +1,21 @@
+c = 'hello world'
+print(c)
+print(c[0])
+print(c[-1])
+
+
+print(c[0:5])
+print(c[2:])
+print(c[:4])
+print(c[2:6:2])
+
+
+string1 = 'hello'
+string2 = 'world'
+new_string = string1 +" "+ string2
+print(new_string)
+
+
+print(sorted(c))
+sorted(c,reverse=True)
+print('it is raining today'.capitalize())
