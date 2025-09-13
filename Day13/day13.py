0876ae9cd09372b939a3ae13e8358badc3a9659f
Vũ Đức Sơn <vuducson1890@gmail.com>
2025-09-13 22:28:38 +0700

    wowwwwww

---
+++ Day13/day13.py
@@ -0,0 +1,32 @@
+a = 3.5
+print(type(a))
+
+print(abs(-4))
+print(pow(2,3))
+print(pow(2,-3))
+
+print(min([2,1,3,0]))
+print(max([2,1,3,0]))
+print (min('kolkata'))
+print(max ('kolkata'))
+
+
+c =22/7
+print(c)
+round(c)
+round(c, 2)
+print('''tren
+duoi''')
+
+
+print(divmod(5,2))
+print(bin(4))
+print(oct(4))
+print(hex(4))
+
+a = 3
+print (id(a))
+print(ord('A'))
+print(len('kolkata'))
+print(len([1, 2, 3]))
+print(sum([1, 2, 3, 4, 5]))
